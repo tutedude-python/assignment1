96f538fe051f97e14522145b4f7c8155029af348
tutedude-python <aitestingmdb@gmail.com>
2025-07-26 10:57:39 +0530

    Add files via upload

---
+++ 1.  Takes two numbers as input from the user..py
@@ -0,0 +1,5 @@
+x=input("Enter First Number :")
+y=input("ENter Second Number :")
+
+
+
---
+++ Assignment 1 Task 3.py
@@ -0,0 +1,5 @@
+x=input ( "Enter First Name :")
+y=input ("Enter Last Name :")
+con= x + " " + y
+print ("Hi welcome ", con)
+
